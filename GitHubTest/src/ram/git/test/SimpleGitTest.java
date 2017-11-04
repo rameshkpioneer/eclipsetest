32e983a693668ef45508fccae99db0b583a5248f
Rameshk <Rameshk@192.168.0.4>
2017-11-04 11:36:25 +0530

    Uploading All the files its first commit

---
+++ GitHubTest/src/ram/git/test/SimpleGitTest.java
@@ -0,0 +1,12 @@
+package ram.git.test;
+
+public class SimpleGitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Ramesh Kolanuvada Testing Project");
+	}
+
+}
---
+++ GitHubTest/src/ram/git/test/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Rameshk
+ *
+ */
+package ram.git.test;
